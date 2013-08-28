67589b5fabef7c3e99af5a4709c97135d286e92c
Adilkhanov Bauyrzhan <abaurd@gmail.com>
2013-08-28 12:13:00 +0800

    Initial commit

---
+++ vendor/bundle/gems/nokogiri-1.6.0/ext/nokogiri/siteconf20130828-1028-98qwle.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130828-1028-1xw0g1"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
